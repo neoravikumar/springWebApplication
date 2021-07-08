c5a72265df159db7711a948c88220d9c55a66ca1
Ravi Kumar <neoravikumar@gmail.com>
2021-07-09 01:32:17 +0530

    Initial Commit

---
+++ src/main/java/com/learn/springWebApplication/SimpleController.java
@@ -0,0 +1,13 @@
+package com.learn.springWebApplication;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Hello World";
+	}
+}
---
+++ src/main/java/com/learn/springWebApplication/SpringWebApplication.java
@@ -0,0 +1,30 @@
+package com.learn.springWebApplication;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("Lets inspect the beans provided by the Spring boot ");
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+		};
+
+	}
+}
